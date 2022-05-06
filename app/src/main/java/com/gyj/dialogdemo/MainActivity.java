a5efa2bced4315d642633f8cc5d4b5e74df5c0be
jiang3131 <2633564497@qq.com>
2022-05-06 18:48:52 +0800

    Crime Second

--- app/src/main/java/com/gyj/dialogdemo/MainActivity.java
+++ app/src/main/java/com/gyj/dialogdemo/MainActivity.java
@@ -8,24 +8,39 @@ import android.view.View;
 import com.gyj.dialogdemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements SetMyTime.DateCallback {
-        ActivityMainBinding binding;
+    ActivityMainBinding binding;
+    private Crime currentCrime;
+    private boolean isUpdate = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.button1.setOnClickListener(new View.OnClickListener() {
+        binding.Date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SetMyTime setMyTime =new SetMyTime();
-                setMyTime.show(getSupportFragmentManager(),"设置时间");
+                SetMyTime setMyTime = new SetMyTime();
+                setMyTime.show(getSupportFragmentManager(), "设置时间");
 
             }
         });
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            currentCrime =(Crime)bundle.get("Crime");
+        }
+        if(currentCrime!=null){
+            isUpdate=true;
+            binding.bigTitle.setText(currentCrime.getBigtitle());
+            binding.Date.setText(currentCrime.getDate());
+            binding.checkBox.setChecked(currentCrime.solved);
+        }
+
+
     }
 
     @Override
     public void getDate(String date) {
-        binding.button1.setText(date);
+        binding.Date.setText(date);
     }
 }
